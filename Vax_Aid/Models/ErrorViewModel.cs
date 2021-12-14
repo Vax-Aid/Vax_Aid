58496fde7289487223481fcd09c0a14380f73537
neschal <nischaluprety12@gmail.com>
2021-12-14 21:35:44 +0545

    Add project files.

---
+++ Vax_Aid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vax_Aid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
